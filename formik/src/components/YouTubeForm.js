bced3da784cc0f22610d9badbcb07411d31233fd
JOANITA-51 <nakityojoanita51@gmail.com>
2022-03-22 19:13:12 +0300

    added onChange prop and value prop

--- formik/src/components/YouTubeForm.js
+++ formik/src/components/YouTubeForm.js
@@ -9,17 +9,19 @@ function YouTubeForm() {
       channel:''
     }
   })
+
+  console.log('formik values', formik.values)
   return (
     <div>
         <form>
             <label htmlFor="name">Name</label>
-            <input type="text" id='name' name='name'/>
+            <input type="text" id='name' name='name' onChange={formik.handleChange} values={formik.values.name}/>
 
             <label htmlFor="channel">Email</label>
-            <input type="email" id='email' name='email'/>
+            <input type="email" id='email' name='email' onChange={formik.handleChange} values={formik.values.email}/>
 
             <label htmlFor="channel">Channel</label>
-            <input type="text" id='channel' name='channel'/>
+            <input type="text" id='channel' name='channel' onChange={formik.handleChange} values={formik.values.channel}/>
 
             <button>Submit</button>
         
