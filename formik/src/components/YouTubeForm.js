76b16760647d9f83ad463a296b2b227d5faa1552
JOANITA-51 <nakityojoanita51@gmail.com>
2022-03-27 15:07:19 +0300

    rendering the error msg only when the form field has been visited

--- formik/src/components/YouTubeForm.js
+++ formik/src/components/YouTubeForm.js
@@ -42,19 +42,19 @@ function YouTubeForm() {
           <div className='form-control'>
             <label htmlFor="name">Name</label>
             <input type="text" id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} values={formik.values.name}/>
-            {formik.errors.name? <div className='error'>{formik.errors.name}</div> : null}
+            {formik.touched.name && formik.errors.name? <div className='error'>{formik.errors.name}</div> : null}
           </div>
 
           <div  className='form-control'>
             <label htmlFor="channel">Email</label>
             <input type="email" id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur}  values={formik.values.email}/>
-            {formik.errors.email? <div className='error'>{formik.errors.email}</div> : null}
+            {formik.touched.email && formik.errors.email? <div className='error'>{formik.errors.email}</div> : null}
           </div>
             
           <div  className='form-control'>
             <label htmlFor="channel">Channel</label>
             <input type="text" id='channel' name='channel' onChange={formik.handleChange} onBlur={formik.handleBlur}  values={formik.values.channel}/>
-            {formik.errors.channel? <div className='error'>{formik.errors.channel}</div> : null}
+            {formik.touched.channel && formik.errors.channel? <div className='error'>{formik.errors.channel}</div> : null}
           </div>  
 
            
