3a5ee295665e86b22182233187c8e65ec2ab5c4c
JOANITA-51 <nakityojoanita51@gmail.com>
2022-03-31 16:05:14 +0300

    textarea

--- formik/src/components/NewYouTubeForm.js
+++ formik/src/components/NewYouTubeForm.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import {Formik, Form, Field} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
 import * as Yup from 'yup'
 
 
 const initialValues = {
   name:'Anita',
   email:'',
-  channel:''
+  channel:'',
+  comment:''
 }
 
 const onSubmit =  values =>{
   console.log('Form data', values)
-}
-
-const validate =  values =>{
-  let errors = {}
-  if (!values.name){
-    errors.name = 'Name is required'
-  }
-  if (!values.email){
-    errors.email = 'Email is required'
-  }else if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(values.email)){
-    errors.email='Invalid email format'
-  }
-  if (!values.channel){
-    errors.channel = 'Channel is required'
-  }
-  return errors
 }
 
 const validationSchema = Yup.object({
@@ -47,21 +32,25 @@ function NewYouTubeForm() {
           <div className='form-control'>
             <label htmlFor="name">Name</label>
             <Field type="text" id='name' name='name'/>
-            {formik.touched.name && formik.errors.name? <div className='error'>{formik.errors.name}</div> : null}
+            <ErrorMessage name='name'/>
           </div>
 
           <div  className='form-control'>
             <label htmlFor="channel">Email</label>
             <Field type="email" id='email' name='email'/>
-            {formik.touched.email && formik.errors.email? <div className='error'>{formik.errors.email}</div> : null}
+            <ErrorMessage name='email'/>
           </div>
             
           <div  className='form-control'>
             <label htmlFor="channel">Channel</label>
-            <Field type="text" id='channel' name='channel'/>
-            {formik.touched.channel && formik.errors.channel? <div className='error'>{formik.errors.channel}</div> : null}
+            <Field type="text" id='channel' name='channel' placeholder='YouTube channel name'/>
+            <ErrorMessage name='email'/>
           </div>  
 
+            <div className="form-control">
+                <label htmlFor="comment">Comment</label>
+                <Field as='textarea' type="text" id='comment' name='comment'/>    
+            </div> 
            
           <button>Submit</button>
         
